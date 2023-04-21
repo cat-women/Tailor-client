2e7f4651286422dc0474d9c14c880f60dfae2b08
cat-women <imlhakpa0000@gmail.com>
2023-04-21 09:59:59 +0545

    Minor changes

--- src/components/Jobs/jobs.js
+++ src/components/Jobs/jobs.js
@@ -1,39 +1,25 @@
-import React, { useState } from "react";
+import React from "react";
 import { Grid, CircularProgress } from "@material-ui/core";
 import { useSelector } from "react-redux";
 
 import Job from "./Job/job.js";
 import useStyle from "./styles.js";
-import Form from "./form.js";
-import JobList from "./jobList.js";
-
+const jobs = [
+  {
+    id: 1,
+    name: "catwomen"
+  }
+];
 export default function Movies() {
   const classess = useStyle();
-  const { data, isLoading } = useSelector(store => store.jobs);
-  const { user } = useSelector(store => store.auth);
-  const userQuotations = useSelector(store => store.quotations.userQuotations);
-  if (isLoading || data == null) return <CircularProgress />;
+  const isLoading = false;
+  if (isLoading) return <CircularProgress />;
   else {
     return (
-      <Grid className={classess.cards} container direction="row" spacing={3}>
-        {user.role_id === "BUYER" &&
-          <div>
-            <Form /> <JobList user={user} />
-          </div>}
-        <Grid
-          item
-          className={classess.card}
-          justifyContent="space-between"
-          alignItems="center"
-          spacing={2}
-          columns={{ xs: 12, sm: 3 }}
-        >
-          {data.map(item => {
-            let quotation;
-            if (userQuotations)
-              quotation = userQuotations.data.find(q => q.job_id === item.id);
-
-            return <Job key={item.id} job={item} quotation={quotation} />;
+      <Grid className={classess.cards}>
+        <Grid item className={classess.card}>
+          {jobs.map(item => {
+            return <Job key={item.id} {...item} />;
           })}
         </Grid>
       </Grid>
