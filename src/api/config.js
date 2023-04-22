0c50f17c2294b5eeddb647822af742fc95daabc1
cat-women <imlhakpa0000@gmail.com>
2023-04-22 22:38:08 +0545

    Adding production url

--- src/api/config.js
+++ src/api/config.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export default function axiosConfigu(token) {
-  const url = "http://127.0.0.1:3333/api";
-
+  // const url = "http://127.0.0.1:3333/api";
+  const url = "https://server-ff9d.onrender.com/api";
   axios.defaults.baseURL = url;
   if (token) {
-    axios.defaults.headers.common["Authorization"] ="Bearer " + token.token;
+    axios.defaults.headers.common["Authorization"] = "Bearer " + token.token;
   }
   axios.defaults.headers.post["Content-Type"] =
     "application/x-www-form-urlencoded";
